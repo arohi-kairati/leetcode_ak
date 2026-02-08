c9847025a22bd3568b0354e40bdb5d057f074bd7
Arohi Kairati <arohikairati44@gmail.com>
2026-02-08 18:37:17 +0530

    Time: 3 ms (77.61%), Space: 46.4 MB (8.87%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -0,0 +1,62 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        int l = nums.length, n = 0;
+        List<String> str = new ArrayList<>();
+        String s = "";
+        for(int i = 0; i < l; i++)
+        {
+            s += String.valueOf(nums[i]);
+            for(int j = i; j < l; j++)
+            {
+                if(j == l-1)
+                {
+                    if(i == j)
+                    {
+                        str.add(s);
+                        System.out.println(str);
+                    }
+                    if(i != j)
+                    {
+                        s += "->" + String.valueOf(nums[j]);
+                        str.add(s);
+                        //System.out.println(str);
+                    }
+                    s = "";
+                    i = j;
+                    break;
+                }
+
+                if(nums[j+1] == nums[j] + 1)
+                {
+                    continue;
+                }
+                else if(nums[j+1] > nums[j] + 1)
+                {
+                    if(i != j)
+                    {
+                        s += "->" + String.valueOf(nums[j]);
+                        //System.out.println(s);
+                        str.add(s);
+                        //System.out.println(str);
+                    }
+                    if(i == j)
+                    {
+                        str.add(s);
+                        //System.out.println(str);
+                    }
+                    s = "";
+                    i = j;
+                    break;
+                }
+                //System.out.println(i+" "+j);
+            }
+            //System.out.println(s);
+            // if(!s.isEmpty())
+            // {
+            //     str.add(s);
+            //     s = "";
+            // }
+        }
+        return str;
+    }
+}
