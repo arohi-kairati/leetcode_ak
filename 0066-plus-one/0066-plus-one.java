7441ffec8f116ff0d4798c8cbb87bdce167146e4
Arohi Kairati <arohikairati44@gmail.com>
2026-02-08 19:47:46 +0530

    Time: 0 ms (100%), Space: 43.3 MB (87.48%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int l = digits.length;
+        for(int i = l-1; i >= 0; i--)
+        {
+            if(digits[i] < 9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int []result = new int[l+1];
+        result[0] = 1;
+        return result;
+    }
+}
