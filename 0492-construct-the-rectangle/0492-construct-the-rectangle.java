f0329d57b62e6731aae6fd2b6f9936e29d452225
Arohi Kairati <arohikairati44@gmail.com>
2026-02-15 18:38:13 +0530

    Time: 1 ms (20.44%), Space: 42.7 MB (26.44%) - LeetHub

---
+++ 0492-construct-the-rectangle/0492-construct-the-rectangle.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] constructRectangle(int area) {
+        int []arr = new int[2];
+        int min = area, j = 0;
+        for(int i = (int)Math.sqrt(area); i >= 1; i--)
+        {
+            if(area % i == 0)
+            {
+                j = area / i;
+                if(j-i < min)
+                {
+                    min = j-i;
+                    arr[0] = j;
+                    arr[1] = i;
+                }
+            }
+        }
+        return arr;
+    }
+}
