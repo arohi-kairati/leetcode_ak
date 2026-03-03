51316cafebc32f41686904ccc1eb46bf9a5bcb9b
Arohi Kairati <arohikairati44@gmail.com>
2026-03-03 17:25:52 +0530

    Time: 195 ms (5.22%), Space: 47.2 MB (9.67%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        int []count1 = new int[1001];
+        int []count2 = new int[1001];
+        int l1 = nums1.length, l2 = nums2.length;
+        for(int i = 0; i < l1; i++)
+        {
+            count1[nums1[i]]++;
+        }
+        for(int i = 0; i < l2; i++)
+        {
+            count2[nums2[i]]++;
+        }
+        int k = 0;
+        int []arr = new int[1001];
+        for(int i = 0; i <= 1000; i++)
+        {
+            System.out.println(count1[i]);
+            System.out.println(count2[i]);
+            int max = 0;
+            if(count1[i] != 0 && count2[i] != 0 && (count1[i] >= count2[i] || count1[i] <= count2[i]))
+            {
+                if(count1[i] > count2[i])
+                {
+                    max = count2[i];
+                }
+                else
+                {
+                    max = count1[i];
+                }
+                for(int j = 0; j < max; j++)
+                {
+                    arr[k] = i;
+                    //System.out.println("uuuu");
+                    k++;
+                }
+            }
+        }
+        int []a = new int[k];
+        for(int i = 0; i < k; i++)
+        {
+            a[i] = arr[i];
+            System.out.println(arr[i]);
+        }
+        return a;
+    }
+}
